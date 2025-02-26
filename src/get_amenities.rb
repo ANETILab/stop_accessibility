faf5ca61aab353211f7ede89db749a92a765e4f6
pintergreg <pintergreg@gmail.com>
2025-02-26 12:12:16 +0100

    Add script to help extracting the amenities from OSM GH-1

---
+++ src/get_amenities.rb
@@ -0,0 +1,18 @@
+require "fileutils"
+
+city = "budapest"
+osm_file = "hungary-20250123.osm.pbf"
+city = "madrid"
+osm_file = "madrid-latest.osm.pbf"
+city = "rotterdam"
+osm_file = "zuid-holland-latest.osm.pbf"
+city = "paris"
+osm_file = "ile-de-france-latest.osm.pbf"
+
+categories = ["amenity", "leisure", "office", "shop", "tourism"]
+categories.each do |category|
+    puts category
+    FileUtils.mkdir_p "../output/#{city}/amenities/"
+    %x(osmium tags-filter -o ../output/#{city}/amenities/#{category}.xml ../data/osm/#{city}/#{osm_file} nwr/#{category})
+    %x(osmium export ../output/#{city}/amenities/#{category}.xml -o ../output/#{city}/amenities/#{category}.geojson -f geojson --attributes type,id --geometry-types point,polygon)
+end
