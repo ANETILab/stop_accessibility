a763dc48afa0d5bc4406664a014eb6f0f86f2883
pintergreg <pintergreg@gmail.com>
2025-03-26 16:07:56 +0100

    Add optional CLI arguments GH-16

--- notebooks/pipeline.rb
+++ notebooks/pipeline.rb
@@ -1,15 +1,22 @@
 require "optparse"
 
-options = {}
+options = {ellipticity_threshold: 5, centrality: "Betweenness Centrality"}
 OptionParser.new do |opts|
     opts.banner = "Usage: pipeline.rb [options]"
 
     opts.on("--city CITY", String, "city identifier (i.e. folder name); lower-case, no spaces") do |x|
         options[:city] = x
     end
+    opts.on("--ellipticity-threshold THRESHOLD", Integer, "number of stops requires to calculate ellipticity") do |x|
+        options[:ellipticity_threshold] = x
+    end
+    opts.on("--centrality CENTRALITY", String, "centrality measure to use, possible values: Eigenvector Centrality, Degree Centrality, Closeness Centrality, Betweenness Centrality") do |x|
+        options[:centrality] = x
+    end
 end.parse!
+
 %x(poetry run python calculate_accessibility.py --city #{options[:city]})
-%x(poetry run python determine_stop_polygons.py --city #{options[:city]})
+%x(poetry run python determine_stop_polygons.py --city #{options[:city]} --ellipticity-threshold #{options[:ellipticity_threshold]})
 %x(poetry run python count_amenities_in_accessibility_polygons.py --city #{options[:city]})
-%x(poetry run python determine_distance_from_center.py --city #{options[:city]})
+%x(poetry run python determine_distance_from_center.py --city #{options[:city]} --centrality "#{options[:centrality]}")
 %x(poetry run python merge_indicators.py --city #{options[:city]})
