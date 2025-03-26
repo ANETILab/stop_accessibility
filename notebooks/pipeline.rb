2b266887045ab5777e68c1e03499e4b329885cc2
pintergreg <pintergreg@gmail.com>
2025-03-26 15:59:14 +0100

    Add CLI to Ruby pipline script GH-16

--- notebooks/pipeline.rb
+++ notebooks/pipeline.rb
@@ -1,6 +1,15 @@
-city = "rotterdam"
-%x(poetry run python calculate_accessibility.py --city #{city})
-%x(poetry run python determine_stop_polygons.py --city #{city})
-%x(poetry run python count_amenities_in_accessibility_polygons.py --city #{city})
-%x(poetry run python determine_distance_from_center.py --city #{city})
-%x(poetry run python merge_indicators.py --city #{city})
+require "optparse"
+
+options = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: pipeline.rb [options]"
+
+    opts.on("--city CITY", String, "city identifier (i.e. folder name); lower-case, no spaces") do |x|
+        options[:city] = x
+    end
+end.parse!
+%x(poetry run python calculate_accessibility.py --city #{options[:city]})
+%x(poetry run python determine_stop_polygons.py --city #{options[:city]})
+%x(poetry run python count_amenities_in_accessibility_polygons.py --city #{options[:city]})
+%x(poetry run python determine_distance_from_center.py --city #{options[:city]})
+%x(poetry run python merge_indicators.py --city #{options[:city]})
