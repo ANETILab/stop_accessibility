ff9f3eed5bc580c32f7c8446ea9c005479672e7b
pintergreg <pintergreg@gmail.com>
2025-04-09 18:10:42 +0200

    Add selectable stages GH-25

--- notebooks/pipeline.rb
+++ notebooks/pipeline.rb
@@ -1,7 +1,7 @@
 require "optparse"
 require "fileutils"
 
-options = {ellipticity_threshold: 5, centrality: "Betweenness Centrality", data_version: ""}
+options = {ellipticity_threshold: 5, centrality: "Betweenness Centrality", data_version: "", stages: ["1", "2", "3", "4", "5"]}
 OptionParser.new do |opts|
     opts.banner = "Usage: pipeline.rb [options]"
 
@@ -17,17 +17,30 @@ OptionParser.new do |opts|
     opts.on("--data-version VERSION", String, "data version (subfolder in city)") do |x|
         options[:data_version] = x
     end
+    opts.on("--stages STAGES", String, "stages separated by comma, i.e.: 1,2,3,4,5") do |x|
+        options[:stages] = x
+    end
 end.parse!
 
 FileUtils.mkdir_p "../output/#{options[:city]}/#{options[:data_version]}"
 
-puts "calculate_accessibility"
-%x(poetry run python calculate_accessibility.py --city #{options[:city]} --data-version "#{options[:data_version]}")
-puts "determine_stop_polygons"
-%x(poetry run python determine_stop_polygons.py --city #{options[:city]} --ellipticity-threshold #{options[:ellipticity_threshold]} --data-version "#{options[:data_version]}")
-puts "count_amenities_in_accessibility_polygons"
-%x(poetry run python count_amenities_in_accessibility_polygons.py --city #{options[:city]} --data-version "#{options[:data_version]}")
-puts "determine_distance_from_center"
-%x(poetry run python determine_distance_from_center.py --city #{options[:city]} --centrality "#{options[:centrality]}" --data-version "#{options[:data_version]}")
-puts "merge_indicators"
-%x(poetry run python merge_indicators.py --city #{options[:city]} --data-version "#{options[:data_version]}")
+if options[:stages].include? "1"
+    puts "[stage 1] calculate_accessibility"
+    %x(poetry run python calculate_accessibility.py --city #{options[:city]} --data-version "#{options[:data_version]}")
+end
+if options[:stages].include? "2"
+    puts "[stage 2] determine_stop_polygons"
+    %x(poetry run python determine_stop_polygons.py --city #{options[:city]} --ellipticity-threshold #{options[:ellipticity_threshold]} --data-version "#{options[:data_version]}")
+end
+if options[:stages].include? "3"
+    puts "[stage 3] count_amenities_in_accessibility_polygons"
+    %x(poetry run python count_amenities_in_accessibility_polygons.py --city #{options[:city]} --data-version "#{options[:data_version]}")
+end
+if options[:stages].include? "4"
+    puts "[stage 4] determine_distance_from_center"
+    %x(poetry run python determine_distance_from_center.py --city #{options[:city]} --centrality "#{options[:centrality]}" --data-version "#{options[:data_version]}")
+end
+if options[:stages].include? "5"
+    puts "[stage 5] merge_indicators"
+    %x(poetry run python merge_indicators.py --city #{options[:city]} --data-version "#{options[:data_version]}")
+end
