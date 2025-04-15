9d05e375f18896d1e10763982f5207824e3164f1
pintergreg <pintergreg@gmail.com>
2025-04-15 09:14:24 +0200

    Use single city argument GH-28

--- notebooks/extract_accessible_stops.py
+++ notebooks/extract_accessible_stops.py
@@ -4,9 +4,9 @@ import pickle
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument(
-    "--cities",
-    nargs="+",
-    default=["budapest", "helsinki", "madrid", "paris", "rotterdam"],
+    "--city",
+    type=str,
+    required=True,
     help="city IDs (lowercase name)",
 )
 argparser.add_argument(
@@ -18,18 +18,17 @@ argparser.add_argument(
 )
 opts = argparser.parse_args()
 
-for city in opts.cities:
-    with open(
-        f"../data/stops/{city}/{opts.data_version}/10min_walbetclus.pkl", "rb"
-    ) as fp:
-        subgraphs = pickle.load(fp)
+with open(
+    f"../data/stops/{opts.city}/{opts.data_version}/10min_walbetclus.pkl", "rb"
+) as fp:
+    subgraphs = pickle.load(fp)
 
-    accessible_stops = {k: list(v[1]) for k, v in subgraphs.items()}
-    if city == "paris":
-        accessible_stops = {
-            str(k): [str(int(i)) for i in v] for k, v in accessible_stops.items()
-        }
-    with open(
-        f"../data/stops/{city}/{opts.data_version}/accessible_stops.json", "w"
-    ) as fp:
-        json.dump(accessible_stops, fp)
+accessible_stops = {k: list(v[1]) for k, v in subgraphs.items()}
+if opts.city == "paris":
+    accessible_stops = {
+        str(k): [str(int(i)) for i in v] for k, v in accessible_stops.items()
+    }
+with open(
+    f"../data/stops/{opts.city}/{opts.data_version}/accessible_stops.json", "w"
+) as fp:
+    json.dump(accessible_stops, fp)
